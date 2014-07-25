73929d89ff740030c15c7e3af7c698aa497e2cb9
Jimmy-Morzaria <morzaria.jimmy@gmail.com>
2014-07-25 20:21:22 +0530

    corrected clustering coefficient

--- src/main/java/org/cytoscape/graph/impl/centralities/ClusteringCoefficientImpl.java
+++ src/main/java/org/cytoscape/graph/impl/centralities/ClusteringCoefficientImpl.java
@@ -3,7 +3,9 @@
  */
 package org.cytoscape.graph.impl.centralities;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.cytoscape.graph.api.centralities.ClusteringCoefficient;
 import org.cytoscape.model.CyEdge;
@@ -12,24 +14,41 @@ import org.cytoscape.model.CyNode;
 
 /**
  * @author Jimmy
- *
+ * 
  */
-public class ClusteringCoefficientImpl implements ClusteringCoefficient{
+public class ClusteringCoefficientImpl implements ClusteringCoefficient {
 
-	public double getClusteringCoefficient(CyNetwork network, CyNode node, boolean directed){
-		
-		List<CyNode> neighborList = network.getNeighborList(node, CyEdge.Type.ANY);
-		int edgeCount = 0;
-		for(CyNode neighbor1 : neighborList){
-			
-			for(CyNode neighbor2 : neighborList){
-				if(neighbor1 == neighbor2)
+	public double getClusteringCoefficient(CyNetwork network, CyNode node,
+			boolean directed) {
+
+		List<CyNode> neighborList;
+		neighborList = network.getNeighborList(node, CyEdge.Type.ANY);
+		double edgeCount = 0.0;
+		Set<CyEdge> edgeSet = new HashSet<CyEdge>();
+		for (CyNode neighbor1 : neighborList) {
+
+			for (CyNode neighbor2 : neighborList) {
+				if (neighbor1 == neighbor2)
 					continue;
-				else if(network.getConnectingEdgeList(neighbor1, neighbor2, CyEdge.Type.ANY).size()!=0){
-					edgeCount++;
+				if (directed) {
+					for (CyEdge edge : network.getConnectingEdgeList(neighbor1,
+							neighbor2, CyEdge.Type.ANY)) {
+						edgeSet.add(edge);
+					}
+				} else {
+					for (CyEdge edge : network.getConnectingEdgeList(neighbor1,
+							neighbor2, CyEdge.Type.ANY)) {
+						edgeCount++;
+						break;
+					}
 				}
 			}
 		}
-		return edgeCount/(neighborList.size()*(neighborList.size()-1));
+		if (directed)
+			return ((double) edgeSet.size() / (neighborList.size() * (neighborList
+					.size() - 1)));
+		else
+			return edgeCount
+					/ (neighborList.size() * (neighborList.size() - 1));
 	}
 }
---
+++ src/test/java/org/cytoscape/graph/centralities/ClusteringCoefficientImplTest.java
@@ -0,0 +1,66 @@
+/**
+ * 
+ */
+package org.cytoscape.graph.centralities;
+
+import static org.junit.Assert.*;
+
+import org.cytoscape.graph.api.centralities.ClusteringCoefficient;
+import org.cytoscape.graph.impl.centralities.ClusteringCoefficientImpl;
+import org.cytoscape.model.CyEdge;
+import org.cytoscape.model.CyNetwork;
+import org.cytoscape.model.CyNode;
+import org.cytoscape.model.NetworkTestSupport;
+import org.junit.Test;
+
+/**
+ * @author Jimmy
+ *
+ */
+public class ClusteringCoefficientImplTest {
+
+	private CyNetwork network, network1;
+	
+	@Test
+	public void testGetClusteringCoefficient(){
+		
+		NetworkTestSupport networkTestSupport = new NetworkTestSupport();
+		network = networkTestSupport.getNetwork();
+		network1 = networkTestSupport.getNetwork();
+		
+		CyNode node1 = network.addNode();
+		CyNode node2 = network.addNode();
+		CyNode node3 = network.addNode();
+		CyNode node4 = network.addNode();
+		
+		CyEdge edge1 = network.addEdge(node1, node2, false);
+		CyEdge edge2 = network.addEdge(node1, node3, false);
+		CyEdge edge3 = network.addEdge(node2, node4, false);
+		CyEdge edge4 = network.addEdge(node3, node4, false);
+		CyEdge edge5 = network.addEdge(node2, node3, false);
+		
+		ClusteringCoefficient cCoefficient = new ClusteringCoefficientImpl();
+		
+		assertEquals(1.0, cCoefficient.getClusteringCoefficient(network, node1, false),0.01);
+		assertEquals(0.66, cCoefficient.getClusteringCoefficient(network, node2, false),0.01);
+		assertEquals(0.66, cCoefficient.getClusteringCoefficient(network, node3, false),0.01);
+		assertEquals(1.0, cCoefficient.getClusteringCoefficient(network, node4, false),0.01);
+		
+		CyNode node_1 = network1.addNode();
+		CyNode node_2 = network1.addNode();
+		CyNode node_3 = network1.addNode();
+		CyNode node_4 = network1.addNode();
+		
+		CyEdge edge_1 = network1.addEdge(node_1, node_2, true);
+		CyEdge edge_2 = network1.addEdge(node_1, node_3, true);
+		CyEdge edge_3 = network1.addEdge(node_4, node_2, true);
+		CyEdge edge_4 = network1.addEdge(node_3, node_4, true);
+		CyEdge edge_5 = network1.addEdge(node_2, node_3, true);
+		
+		assertEquals(0.5, cCoefficient.getClusteringCoefficient(network1, node_1, true),0.01);
+		assertEquals(0.33, cCoefficient.getClusteringCoefficient(network1, node_2, true),0.01);
+		assertEquals(0.33, cCoefficient.getClusteringCoefficient(network1, node_3, true),0.01);
+		assertEquals(0.5, cCoefficient.getClusteringCoefficient(network1, node_4, true),0.01);
+		
+	}
+}
